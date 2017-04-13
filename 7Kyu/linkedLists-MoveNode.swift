40e3582ca1084088e41a40f7444db7d487f0734d
JWNg <JWNg@users.noreply.github.com>
2017-04-13 06:03:59 -0400

    Update linkedLists-MoveNode.swift

--- 7Kyu/linkedLists-MoveNode.swift
+++ 7Kyu/linkedLists-MoveNode.swift
@@ -50,3 +50,25 @@ Linked Lists - Recursive Reverse
 
 Inspired by Stanford Professor Nick Parlante's excellent Linked List teachings.
 FUNDAMENTALSLINKED LISTSLISTSDATA STRUCTURES
+
+class Node {
+    var data: Int
+    var next: Node?
+    init(_ data: Int) {
+        self.data = data
+    }
+}
+
+enum moveNodeError: Error {
+    case sourceListIsEmpty
+}
+
+struct Context {
+    var source:Node?
+    var dest:Node?
+}
+
+func moveNode(source:Node?, dest:Node?) throws -> Context? {
+    guard source != nil else {throw moveNodeError.sourceListIsEmpty}
+    return Context(source: source?.next, dest: push(dest, (source?.data)!))
+}

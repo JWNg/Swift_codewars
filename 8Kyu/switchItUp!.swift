e8e680316947a4425ed36885a3a09690870f85ad
JWNg <JWNg@users.noreply.github.com>
2017-04-21 07:03:36 -0400

    Update switchItUp!.swift

--- 8Kyu/switchItUp!.swift
+++ 8Kyu/switchItUp!.swift
@@ -8,3 +8,23 @@ Try using "Switch" statements.
 
 This kata is meant for beginners. Rank and upvote to bring it out of beta
 FUNDAMENTALS
+
+func switchItUp(_ number: Int) -> String {
+  switch number {
+  case 0: return "Zero"
+  case 1: return "One"
+  case 2: return "Two"
+  case 3: return "Three"
+  case 4: return "Four"
+  case 5: return "Five"
+  case 6: return "Six"
+  case 7: return "Seven"
+  case 8: return "Eight"
+  case 9: return "Nine"
+  default: return "Unknown"
+  }
+}
+
+func switchItUp(_ number: Int) -> String {
+  return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][number]
+}
